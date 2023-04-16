932a46797d45d506835221ac70c38128db3eb834
Merve Dogan-Espaillat <1mervedogan@gmail.com>
2023-04-16 15:08:27 -0400

    latest version

--- ninety-nine-cats/app/controllers/cats_controller.rb
+++ ninety-nine-cats/app/controllers/cats_controller.rb
@@ -11,10 +11,19 @@ class CatsController < ApplicationController
     end
 
     def edit
+        @cat = Cat.find_by(id: params[:id])
+        render :edit
 
     end
 
     def update
+        @cat = Cat.find_by(id: params[:id])
+        
+        if @cat.update(cat_params)
+            redirect_to cat_url(@cat)
+        else    
+            render json: @cat.errors.full_messages, status: 422
+        end
     end
 
     def new
