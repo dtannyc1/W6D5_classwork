9244adc19f058d569a5dc3d6f473103494e36af3
dtannyc1 <dtannyc1@gmail.com>
2023-04-16 16:54:26 -0400

    Finished validation for CatRentalRequests#overlapping_approved_requests

--- ninety-nine-cats/app/models/cat_rental_request.rb
+++ ninety-nine-cats/app/models/cat_rental_request.rb
@@ -2,6 +2,7 @@ class CatRentalRequest < ApplicationRecord
     validates(:status, inclusion: {in: ['PENDING', 'APPROVED', 'DENIED'],
                                    message: "Invalid Status"})
     validates(:start_date, :end_date, :cat_id, presence: true)
+    validate(:does_not_overlap_approved_request)
 
     belongs_to(
         :cat,
@@ -10,8 +11,15 @@ class CatRentalRequest < ApplicationRecord
         class_name: :Cat
     )
 
+    def does_not_overlap_approved_request
+        if overlapping_approved_requests.exists?
+            errors.add("Cat is already booked on these rental dates")
+        end
+    end
+
     def overlapping_requests
-        CatRentalRequest.where.not("end_date < ? OR start_date > ?", self.start_date, self.end_date)
+        CatRentalRequest.where(cat_id: self.cat_id)
+            .where.not("end_date < ? OR start_date > ?", self.start_date, self.end_date)
     end
 
     def overlapping_approved_requests
--- ninety-nine-cats/db/seeds.rb
+++ ninety-nine-cats/db/seeds.rb
@@ -8,14 +8,67 @@
 
 require 'faker'
 
+puts "Destroying Tables..."
 Cat.destroy_all
+CatRentalRequest.destroy_all
 
 ApplicationRecord.connection.reset_pk_sequence!("cats")
+ApplicationRecord.connection.reset_pk_sequence!("cat_rental_requests")
 
+puts "Creating 100 cats..."
 100.times do
-    Cat.create([{ name: Faker::Creature::Cat.name,
+    Cat.create(name: Faker::Creature::Cat.name,
                   color: ["black", "white", "orange", "gray", "blue"].sample,
                   sex: ["M", "F"].sample,
                   birth_date: "1/1/2023",
-                  description: Faker::Quote.most_interesting_man_in_the_world} ])
+                  description: Faker::Quote.most_interesting_man_in_the_world)
 end
+
+# 5.times do
+#     start_date = Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+# end
+
+# puts "Running CatRentalRequests Tests..."
+# test_1 = CatRentalRequest.create(
+#     cat_id: 1,
+#     start_date: Date.today,
+#     end_date: Date.today + 7,
+#     status: 'APPROVED'
+# )
+
+# test_2 = CatRentalRequest.new(
+#     cat_id: 1,
+#     start_date: Date.today - 2,
+#     end_date: Date.today + 2
+# )
+
+# test_3 = CatRentalRequest.new(
+#     cat_id: 2,
+#     start_date: Date.today + 6,
+#     end_date: Date.today + 9
+# )
+
+# test_4 = CatRentalRequest.new(
+#     cat_id: 1,
+#     start_date: Date.today + 8,
+#     end_date: Date.today + 9
+# )
+
+# test_5 = CatRentalRequest.new(
+#     cat_id: 1,
+#     start_date: Date.today - 7,
+#     end_date: Date.today - 3
+# )
+
+
+# p test_2.overlapping_approved_requests
+# puts "^ should have one item"
+
+# p test_3.overlapping_approved_requests
+# puts "^ different cat, should be empty"
+
+# p test_4.overlapping_approved_requests
+# puts "^ no overlap, should be empty"
+
+# p test_5.overlapping_approved_requests
+# puts "^ no overlap, should be empty"
